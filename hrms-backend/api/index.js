a11dfd19a72b0d0a47325fe443306ef6fb9c2c6f
kudikalar <rameshkudikala72@gmail.com>
2026-02-13 17:25:27 +0530

    added both files index and vercel

---
+++ hrms-backend/api/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/test", (req, res) => {
+  res.json({ message: "Backend Working ✅" });
+});
+
+export default app;
